4ae5344f847b27cba7f4f8c636854d45684ad638
beautyhasnoshape <beautyhasnoshape@gmail.com>
2018-12-14 20:38:39 +0100

    solution for Day 14

---
+++ src/test/java/com/beautyhasnoshape/aoc2018/Day14Test.java
@@ -0,0 +1,82 @@
+package com.beautyhasnoshape.aoc2018;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Day14Test {
+    private Day14 testObj;
+
+    @Before
+    public void setUp() {
+        testObj = new Day14();
+    }
+
+    @Test
+    public void shouldSolvePartA() throws Exception {
+        // given
+
+        // when
+        String result = testObj.solvePartA("37", 768071);
+
+        // then
+        Assert.assertEquals("6548103910", result);
+    }
+
+    @Test
+    public void shouldSolvePartB() throws Exception {
+        // given
+
+        // when
+        String result = testObj.solvePartB("37", new int[] { 7, 6, 8, 0, 7, 1 });
+
+        // then
+        Assert.assertEquals("20198090", result);
+    }
+
+    @Test
+    public void shouldSolvePartASample1() throws Exception {
+        // given
+
+        // when
+        String result = testObj.solvePartA("37", 5);
+
+        // then
+        Assert.assertEquals("0124515891", result);
+    }
+
+    @Test
+    public void shouldSolvePartASample2() throws Exception {
+        // given
+
+        // when
+        String result = testObj.solvePartA("37", 9);
+
+        // then
+        Assert.assertEquals("5158916779", result);
+    }
+
+    @Test
+    public void shouldSolvePartASample3() throws Exception {
+        // given
+
+        // when
+        String result = testObj.solvePartA("37", 18);
+
+        // then
+        Assert.assertEquals("9251071085", result);
+    }
+
+    @Test
+    public void shouldSolvePartASample4() throws Exception {
+        // given
+
+        // when
+        String result = testObj.solvePartA("37", 2018);
+
+        // then
+        Assert.assertEquals("5941429882", result);
+    }
+
+
+}
